3ce22078453b12d1896615098562ade7248eb602
akingyin1987 <akingyin@163.com>
2016-02-18 18:12:28 +0800

    rxcamera 这相机有点瓿

--- app/src/main/java/com/akingyin/ui/fragment/RxViewFragment.java
+++ app/src/main/java/com/akingyin/ui/fragment/RxViewFragment.java
@@ -1,30 +1,48 @@
 package com.akingyin.ui.fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.akingyin.presenter.IRxbindPresenter;
 import com.akingyin.presenter.impl.RxBindPresenterImpl;
 import com.akingyin.view.IRxBindView;
 import com.jakewharton.rxbinding.view.RxView;
 import com.md.multipleapp.R;
+import com.ragnarok.rxcamera.RxCamera;
+import com.ragnarok.rxcamera.RxCameraData;
+import com.ragnarok.rxcamera.config.RxCameraConfig;
+import com.ragnarok.rxcamera.config.RxCameraConfigChooser;
+import com.ragnarok.rxcamera.request.Func;
 
 import org.apache.commons.lang.RandomStringUtils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 
 import co.mobiwise.materialintro.shape.Focus;
 import co.mobiwise.materialintro.shape.FocusGravity;
 import co.mobiwise.materialintro.view.MaterialIntroView;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.functions.Func1;
 
 /**
  * Created by zlcd on 2016/2/4.
@@ -33,7 +51,8 @@ public class RxViewFragment extends Fragment implements IRxBindView{
 
     IRxbindPresenter   presenter;
     public Button   rxview_one_btn,rxview_tow_btn,rxview_each;
-
+    public  TextureView rxb_texture;
+    private RxCamera camera;
     public TextView  rxview_info;
     public   static   RxViewFragment   newInstance(){
         RxViewFragment   fragment = new RxViewFragment();
@@ -70,6 +89,7 @@ public class RxViewFragment extends Fragment implements IRxBindView{
         rxview_tow_btn = (Button)view.findViewById(R.id.rxview_tow_btn);
         rxview_info = (TextView)view.findViewById(R.id.rxview_info);
         rxview_each = (Button)view.findViewById(R.id.rxview_each);
+        rxb_texture= (TextureView)view.findViewById(R.id.rxb_texture);
 
         RxView.clicks(rxview_each).throttleFirst(1,TimeUnit.SECONDS)
               .subscribe(new Action1<Void>() {
@@ -84,8 +104,38 @@ public class RxViewFragment extends Fragment implements IRxBindView{
               .subscribe(new Action1<Void>() {
                   @Override
                   public void call(Void aVoid) {
-                      rxview_one_btn.setText("one"+ RandomStringUtils.random(4,"我是中国人我爱您"));
-                      showMessage(rxview_one_btn,"testOne");
+                      if(null == camera){
+                          openCamera();
+                          return;
+                      }
+                    //  rxview_one_btn.setText("one"+ RandomStringUtils.random(4,"我是中国人我爱您"));
+                    //  showMessage(rxview_one_btn,"testOne");
+                      if(null != camera){
+                          camera.request().takePictureRequest(true, new Func() {
+                              @Override
+                              public void call() {
+
+                              }
+                          },960,540, ImageFormat.JPEG)
+                              .subscribe(new Action1<RxCameraData>() {
+                              @Override
+                              public void call(RxCameraData rxCameraData) {
+                                  String path = Environment.getExternalStorageDirectory() + "/test.jpg";
+                                  File file = new File(path);
+                                  Bitmap bitmap = BitmapFactory.decodeByteArray(rxCameraData.cameraData, 0, rxCameraData.cameraData.length);
+                                  bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(),
+                                      rxCameraData.rotateMatrix, false);
+                                  try {
+                                      file.createNewFile();
+                                      FileOutputStream fos = new FileOutputStream(file);
+                                      bitmap.compress(Bitmap.CompressFormat.JPEG, 80, fos);
+                                      fos.close();
+                                  } catch (IOException e) {
+                                      e.printStackTrace();
+                                  }
+                              }
+                          });
+                      }
                   }
               });
 
@@ -95,9 +145,49 @@ public class RxViewFragment extends Fragment implements IRxBindView{
                 @Override
                 public void call(Void aVoid) {
                     rxview_one_btn.setText("tow"+ RandomStringUtils.random(4,"我是中国人我爱您"));
-                    showMessage(rxview_tow_btn,"testTow");
                 }
             });
+
+
+    }
+
+
+    public   void  openCamera(){
+        RxCameraConfig config = RxCameraConfigChooser.obtain().
+            useBackCamera().
+            setAutoFocus(true).
+            setPreferPreviewFrameRate(15, 30).
+            setPreferPreviewSize(new Point(960, 540)).
+            setHandleSurfaceEvent(true).
+            get();
+        RxCamera.open(getContext(),config).flatMap(new Func1<RxCamera, Observable<RxCamera>>() {
+            @Override
+            public Observable<RxCamera> call(RxCamera rxCamera) {
+                return rxCamera.bindTexture(rxb_texture);
+            }
+        }).flatMap(new Func1<RxCamera, Observable<RxCamera>>() {
+            @Override
+            public Observable<RxCamera> call(RxCamera rxCamera) {
+                return rxCamera.startPreview();
+            }
+        }).observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Subscriber<RxCamera>() {
+            @Override
+            public void onCompleted() {
+                 System.out.println("completed");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                 System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onNext(RxCamera rxCamera) {
+                  System.out.println("next");
+                camera = rxCamera;
+            }
+        });
     }
 
     @Override
@@ -130,5 +220,13 @@ public class RxViewFragment extends Fragment implements IRxBindView{
     @Override
     public void printMessage(String message) {
         rxview_info.setText(message);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(null != camera){
+            camera.closeCamera();
+        }
     }
 }
