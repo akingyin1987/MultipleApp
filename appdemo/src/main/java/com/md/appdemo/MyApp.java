4973188cac551c883ef705a3dfc7ca7f215b17db
akingyin1987 <akingyin@163.com>
2016-04-06 18:39:08 +0800

    加入dagger2+butterknife ，准备用来做MVP

--- appdemo/src/main/java/com/md/appdemo/MyApp.java
+++ appdemo/src/main/java/com/md/appdemo/MyApp.java
@@ -1,10 +1,13 @@
 package com.md.appdemo;
 
 import android.app.Application;
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.widget.Toast;
 
 import com.facebook.stetho.Stetho;
+import com.md.appdemo.center.AppComponent;
+import com.md.appdemo.center.AppModule;
 
 
 /**
@@ -19,11 +22,19 @@ public class MyApp extends Application {
     }
 
 
+    private AppComponent appComponent;
+
+    public static MyApp get(Context context){
+        return (MyApp)context.getApplicationContext();
+    }
+
+
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
+
         Toast.makeText(this,"初始化子Application",Toast.LENGTH_SHORT).show();
 
         Stetho.initialize(Stetho.newInitializerBuilder(this)
