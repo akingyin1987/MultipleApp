0fe2e1aab555cfa48721327d15fda7ac3cd22703
akingyin1987 <akingyin@163.com>
2016-05-11 18:36:45 +0800

    准备研究单元测试

--- akingyinlib/src/test/java/com/akingyin/sharelibs/ExampleUnitTest.java
+++ akingyinlib/src/test/java/com/akingyin/sharelibs/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.akingyin.sharelibs;
 
+import android.text.TextUtils;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,8 +9,11 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+
+    public   static   String  key="124";
     @Test
     public void addition_isCorrect() throws Exception {
+        assertEquals(TextUtils.isEmpty(key),false);
         assertEquals(4, 2 + 2);
     }
 }
