38632d74c020801f0a34a0bc2537afee6c4bdd57
akingyin1987 <akingyin@163.com>
2016-05-17 16:29:17 +0800

    添加各种加密解密算法

--- appdemo/src/main/java/com/md/appdemo/jobmanager/AsyncJob.java
+++ appdemo/src/main/java/com/md/appdemo/jobmanager/AsyncJob.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *   Copyright (c) 2016 [akingyin@163.com]
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License”);
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
 package com.md.appdemo.jobmanager;
 
 /**
@@ -52,18 +70,61 @@ public abstract class AsyncJob<T>  implements  Runnable {
 
     //执行任务
     public   abstract    void     doOnBackground();
+    
+    //下一次
+    public   abstract    JobstatusEnum      onNext();
 
-
+    //之前处理
+    public   abstract    boolean     onBefore();
+    
+    //之后
+    public   abstract    void    onAfter();
+    
     @Override
     public void run() {
            if(jobstatusEnum == JobstatusEnum.Null){
-               doOnBackground();
+        	   if(onBefore()){  
+        		   JobstatusEnum  nextstatus = JobstatusEnum.Runing;
+        		   while((nextstatus = onNext()) == JobstatusEnum.Runing);
+        		   if(nextstatus == JobstatusEnum.Success){
+        			   doOnBackground();
+        			   onAfter();
+        		   }else{
+        			   onError();
+        			   return;
+        		   }
+        		  
+        	   }else{
+        		   onError();
+        	   }
+              
            }
     }
 
 
     //任务监听器
     public   interface   OnJobListion{
-        void   onResult(JobstatusEnum  jobstatusEnum);
+        void   onResult(JobstatusEnum jobstatusEnum);
+    }
+    
+    public  void   onSuccess(){
+    	jobstatusEnum = JobstatusEnum.Success;
+    	if(null != jobListion){
+    		jobListion.onResult(JobstatusEnum.Success);
+    	}
+    }
+    
+    public  void   onError(){
+    	jobstatusEnum = JobstatusEnum.Error;
+    	if(null != jobListion){
+    		jobListion.onResult(JobstatusEnum.Error);
+    	}
+    }
+    
+    public  void   onNetWorkError(){
+    	jobstatusEnum = JobstatusEnum.NetWorkError;
+    	if(null != jobListion){
+    		jobListion.onResult(JobstatusEnum.NetWorkError);
+    	}
     }
 }
--- appdemo/src/main/java/com/md/appdemo/jobmanager/AsyncJobManager.java
+++ appdemo/src/main/java/com/md/appdemo/jobmanager/AsyncJobManager.java
@@ -1,4 +1,23 @@
+/*
+ *
+ *   Copyright (c) 2016 [akingyin@163.com]
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License”);
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
 package com.md.appdemo.jobmanager;
+
 
 import android.os.Handler;
 import android.os.Looper;
@@ -13,23 +32,42 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * Created by Administrator on 2016/1/19.
  */
-public  class AsyncJobManager<T>  implements AsyncJob.OnJobListion ,Runnable{
+public class AsyncJobManager<T>  implements AsyncJob.OnJobListion ,Runnable{
 
     public   static   final   int   nThreads = 3;
 
-    private   final LinkedList<AsyncJob<T>>    jobLinkedList = new LinkedList<>();
+    private   final LinkedList<AsyncJob<T>>    jobLinkedList = new LinkedList<AsyncJob<T>>();
 
     private ExecutorService executorService;
 
     private  OnJobManagerListion    listion;
+    
+    
 
-    //任务管理状态
+    public OnJobManagerListion getListion() {
+		return listion;
+	}
+
+	public void setListion(OnJobManagerListion listion) {
+		this.listion = listion;
+	}
+
+	//任务管理状态
     private AtomicBoolean   jobmanagerstatus = new AtomicBoolean(false);
 
 
     //当前任务总数
     public    int   getJobTotal(){
         return  jobLinkedList.size();
+    }
+    
+    public    void  cleanAllJobs(){
+    	if(!jobmanagerstatus.get()){
+    		 for(AsyncJob<T>  job : jobLinkedList){
+    	            job.onCancel();
+    	        }
+    	 jobLinkedList.clear();
+    	}
     }
 
     private AtomicInteger progress = new AtomicInteger(0);
@@ -39,7 +77,7 @@ public  class AsyncJobManager<T>  implements AsyncJob.OnJobListion ,Runnable{
     //当前任务失败数
     public  int   getErrorTotal(){
         int  eror = 0;
-        for(AsyncJob  job : jobLinkedList){
+        for(AsyncJob<T>  job : jobLinkedList){
             if(job.getJobstatusEnum() == JobstatusEnum.Error){
                 eror++;
             }
@@ -49,7 +87,7 @@ public  class AsyncJobManager<T>  implements AsyncJob.OnJobListion ,Runnable{
 
     public  int  getProress(){
         int  proress = 0;
-        for(AsyncJob  job : jobLinkedList){
+        for(AsyncJob<T>  job : jobLinkedList){
             if(job.getJobstatusEnum() != JobstatusEnum.Null){
                 proress++;
             }
@@ -87,10 +125,10 @@ public  class AsyncJobManager<T>  implements AsyncJob.OnJobListion ,Runnable{
     //执行任务
     public  synchronized   void    execute(){
         if(!jobmanagerstatus.get()){
-            progress.set(getProress());
-            error.set(getErrorTotal());
+            progress.getAndSet(getProress());
+            error.getAndSet(getErrorTotal());
             jobmanagerstatus.set(true);
-            for(AsyncJob  job : jobLinkedList){
+            for(AsyncJob<T>  job : jobLinkedList){
                 if(job.getJobstatusEnum() == JobstatusEnum.Null){
                     job.setJobListion(this);
                     executorService.execute(job);
@@ -101,40 +139,55 @@ public  class AsyncJobManager<T>  implements AsyncJob.OnJobListion ,Runnable{
 
     //结束
     public synchronized  void  onDestory(){
-        jobmanagerstatus.set(false);
-        executorService.shutdown();
-        for(AsyncJob  job : jobLinkedList){
-            job.onCancel();
+        try{
+            jobmanagerstatus.getAndSet(false);
+            executorService.shutdown();
+            for(AsyncJob<T>  job : jobLinkedList){
+                job.onCancel();
+            }
+            jobLinkedList.clear();
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        jobLinkedList.clear();
+
 
     }
 
     public interface    OnJobManagerListion{
-        void  onProgress(int  total,int press,int error);
+        void  onProgress(int total, int press, int error);
 
         void  onError(String message);
+
+        void  onComplete();
     }
 
-    private static Handler uiHandler = new Handler(Looper.getMainLooper());
+   //private static Handler uiHandler = new Handler(Looper.getMainLooper());
     @Override
     public void onResult(JobstatusEnum jobstatusEnum) {
+    	
         progress.set(progress.get()+1);
+       
         if(jobstatusEnum == JobstatusEnum.NetWorkError){
-            jobmanagerstatus.set(false);
+            jobmanagerstatus.getAndSet(false);
             onDestory();
+            if(null != listion){
+            	listion.onError("网络错误，请稍候再试");
+            }
             return;
         }
         if(jobstatusEnum == JobstatusEnum.Error){
-            error.set(error.get() + 1);
+            error.getAndAdd(1);
         }
         if(null != listion){
 
-            uiHandler.post(this);
+        	 listion.onProgress(getJobTotal(),progress.get(),error.get());
         }
         if(progress.get() == getJobTotal()){
-            jobmanagerstatus.set(false);
+            jobmanagerstatus.getAndSet(false);
             onDestory();
+            if(null != listion){
+                listion.onComplete();
+            }
         }
 
     }
